313bfe3ceefb6c66fa3d9aa74640d027a3620090
apoc64 <sschwedt@gmail.com>
2018-04-17 10:50:37 -0600

    tests it wont load bad data

--- test/company_test.rb
+++ test/company_test.rb
@@ -22,15 +22,12 @@ class CompanyTest < Minitest::Test
     assert_equal 2, company.employees.count
     assert_instance_of Employee, company.employees[0]
   end
-    # Parameters: String
-    # Returns: Hash
-    # Read all data from filename in CSV format. Only add its contents to company.employees if ALL data in the file is valid. If any data is invalid, you should immediately return an error INSTEAD of saving the data. A test file for bad employee data has been provided.
-    #
-    # This method should return a hash. A success key in the hash will contain a boolean letting us know if the whole file imported correctly or not. An error key, if success is false, will tell us why the import failed.
-    #
-    # Return value examples:
-    #
-    # {success: true, error: nil}
-    # {success: false, error: 'bad data'}
 
+  def test_it_will_not_load_bad_data
+      company = Company.new
+      actual = company.load_employees('./data/bad_employees.csv')
+      expected = {success: false, error: 'bad data'}
+      assert_equal expected, actual
+      assert_equal [], company.employees
+  end
 end
