c7ea5509297d5fdda4c8ebb5843ee9ab84d672f5
apoc64 <sschwedt@gmail.com>
2018-04-17 09:29:32 -0600

    tests employee attributes

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,15 @@
 class Employee
+  attr_reader :id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+
+  def initialize(id, name, role, start_date, end_date)
+    @id = id
+    @name = name
+    @role = role
+    @start_date = start_date
+    @end_date = end_date
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,9 +2,30 @@ require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
+  def setup
+    employee_id = '5'
+    name = 'Sally Jackson'
+    role = 'Engineer'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    @employee = Employee.new(employee_id, name, role, start_date, end_date)
+  end
   def test_instantiation
+    assert_instance_of Employee, @employee
   end
 
   def test_attributes
+    # skip
+  # make sure to convert all ID values to integers
+  assert_instance_of Integer, @employee.id.class
+  # => Integer
+  assert_equal "Sally Jackson", @employee.name
+  # => "Sally Jackson"
+  assert_equal "Engineer", @employee.role
+  # => "Engineer"
+  assert_instance_of Time, @employee.start_date
+  # => #<Date: 2015-01-01 ((2457024j,0s,0n),+0s,2299161j)>
+  assert_instance_of Time, @employee.end_date
+  # => #<Date: 2018-01-01 ((2458120j,0s,0n),+0s,2299161j)>
   end
 end
