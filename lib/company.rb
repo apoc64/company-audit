1342058d6c059a25ab875da456de05bd18ad496f
apoc64 <sschwedt@gmail.com>
2018-04-17 11:12:37 -0600

    helper methods with testing for load methods

--- lib/company.rb
+++ lib/company.rb
@@ -13,13 +13,33 @@ class Company
   end
 
   def load_employees(filename)
-    failure = {success: false, error: 'bad data'}
     records = CSV.read(filename)
+    return failure if !check_validity(records, 5)
     records.each do |rec|
-      return failure if rec.length != 5
       employee = Employee.new(rec[0], rec[1], rec[2], rec[3], rec[4])
       @employees << employee
     end
     {success: true, error: nil}
   end
+
+  def load_projects(filename)
+    records = CSV.read(filename)
+    return failure if !check_validity(records, 4)
+    records.each do |rec|
+      project = Project.new(rec[0], rec[1], rec[2], rec[3])
+      @projects << project
+    end
+    {success: true, error: nil}
+  end
+
+  def check_validity(records, count)
+    records.all? do |record|
+      record.length == count
+    end
+  end
+
+  def failure
+    {success: false, error: 'bad data'}
+  end
+
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -23,11 +23,42 @@ class CompanyTest < Minitest::Test
     assert_instance_of Employee, company.employees[0]
   end
 
-  def test_it_will_not_load_bad_data
+  def test_it_will_not_load_bad_employee_data
       company = Company.new
       actual = company.load_employees('./data/bad_employees.csv')
       expected = {success: false, error: 'bad data'}
       assert_equal expected, actual
       assert_equal [], company.employees
+  end
+
+  def test_it_loads_projects
+    company = Company.new
+    actual = company.load_projects('./data/projects.csv')
+    expected = {success: true, error: nil}
+    assert_equal expected, actual
+    assert_equal 3, company.projects.count
+    assert_instance_of Project, company.projects[0]
+  end
+
+  def test_it_will_not_load_bad_project_data
+      company = Company.new
+      actual = company.load_projects('./data/bad_projects.csv')
+      expected = {success: false, error: 'bad data'}
+      assert_equal expected, actual
+      assert_equal [], company.projects
+  end
+
+  def test_it_checks_validity
+    company = Company.new
+    good = [[1,2,3],[1,2,3]]
+    bad = [[1,2,3],[1,2],[1,2,3]]
+    assert company.check_validity(good, 3)
+    refute company.check_validity(bad, 3)
+  end
+
+  def test_it_can_return_failure
+    company = Company.new
+    expected = {success: false, error: 'bad data'}
+    assert_equal expected, company.failure
   end
 end
