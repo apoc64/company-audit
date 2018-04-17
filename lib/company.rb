8c2aeb032e21d4215fb696c7dda6c515d1a3df9f
apoc64 <sschwedt@gmail.com>
2018-04-17 10:56:08 -0600

    formatting changes on load employee

--- lib/company.rb
+++ lib/company.rb
@@ -13,14 +13,11 @@ class Company
   end
 
   def load_employees(filename)
+    failure = {success: false, error: 'bad data'}
     records = CSV.read(filename)
-    records.each do |record|
-      id = record[0]
-      name = record[1]
-      role = record[2]
-      start_date = record[3]
-      end_date = record[4]
-      employee = Employee.new(id, name, role, start_date, end_date)
+    records.each do |rec|
+      return failure if rec.length != 5
+      employee = Employee.new(rec[0], rec[1], rec[2], rec[3], rec[4])
       @employees << employee
     end
     {success: true, error: nil}
